900682de544ea6645c1b76f45d16cceae11ad29d
Dorian Szatmari <dorian.szatmari@gmail.com>
2023-02-14 21:12:05 +0100

    DbContext DI, Add method

---
+++ Backend/MentalHealth/Repository/TaskRepository.cs
@@ -0,0 +1,42 @@
+﻿using MentalHealth.Models.Entities;
+
+namespace MentalHealth.Repository;
+
+public class TaskRepository : IRepository<UserTask>
+{
+    private readonly MentalHealthContext _context;
+
+    public TaskRepository(MentalHealthContext context)
+    {
+        _context = context;
+    }
+
+    public async Task Add(UserTask entity)
+    {
+        using (_context)
+        {
+            await _context.Tasks.AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+    }
+
+    public Task<UserTask> Get(long id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<UserTask>> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task Update(long id, UserTask entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task Delete(long id)
+    {
+        throw new NotImplementedException();
+    }
+}
