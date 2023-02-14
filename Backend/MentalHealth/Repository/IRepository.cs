a9127cd0cfdeb0061dad55ef9afc8e96450d077a
Dorian Szatmari <dorian.szatmari@gmail.com>
2023-02-14 18:41:39 +0100

    Add Repository interface

---
+++ Backend/MentalHealth/Repository/IRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace MentalHealth.Repository;
+
+public interface IRepository<T>
+{
+    public Task<T> Add(T entity);
+    public Task<T> Get(long id);
+    public Task<IEnumerable<T>> GetAll();
+    public Task<T> Update(long id, T entity);
+    public Task<T> Delete(long id);
+}
