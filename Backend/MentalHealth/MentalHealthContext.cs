29fb3df0d55f046419bc76f8bd98b5844b99068e
Akos <barcza.akos15@gmail.com>
2023-02-15 21:04:12 +0100

    Add MoodTracker DbSet to MentalHealthContext

--- Backend/MentalHealth/MentalHealthContext.cs
+++ Backend/MentalHealth/MentalHealthContext.cs
@@ -1,8 +1,12 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using MentalHealth.Models.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace MentalHealth;
 
 public class MentalHealthContext : DbContext
 {
-    
+    public DbSet<MoodTracker> MoodTracker { get; set; }
+	public MentalHealthContext(DbContextOptions<MentalHealthContext> options) : base(options)
+	{
+	}
 }
