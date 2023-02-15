ea2a212eb58ef47b2ddaa51072bd2c22b7fa8df5
Dorian Szatmari <dorian.szatmari@gmail.com>
2023-02-15 18:50:04 +0100

    Implement AddUser endpoint

---
+++ Backend/MentalHealth/Controllers/UserTaskController.cs
@@ -0,0 +1,23 @@
+﻿using MentalHealth.Models.Entities;
+using MentalHealth.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MentalHealth.Controllers;
+
+[ApiController, Route("/usertask")]
+public class UserTaskController : ControllerBase
+{
+    private readonly TaskRepository _repository;
+
+    public UserTaskController(TaskRepository repository)
+    {
+        _repository = repository;
+    }
+
+    [HttpPost]
+    public async Task AddUserTask(UserTask userTask)
+    {
+        await _repository.Add(userTask);
+    }
+
+}
