7989efdb0589c67677977ddec1c006453e8d0c3c
Attila Dobi <attiladobi100@gmail.com>
2023-02-15 14:51:34 +0100

    Added Trees prop to Context

--- Backend/MentalHealth/MentalHealthContext.cs
+++ Backend/MentalHealth/MentalHealthContext.cs
@@ -6,8 +6,10 @@ namespace MentalHealth;
 public class MentalHealthContext : DbContext
 {
     public DbSet<UserTask> Tasks { get; set; }
-    
+    public DbSet<Tree> Trees { get; set; }
     public MentalHealthContext(DbContextOptions<MentalHealthContext> options) : base(options)
     {
     }
+    
+    
 }
