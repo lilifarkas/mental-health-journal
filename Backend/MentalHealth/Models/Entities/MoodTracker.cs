2d486cb03f50212bc492b7e488a2a7b9fb4576a9
Akos <barcza.akos15@gmail.com>
2023-02-15 01:15:53 +0100

    Create MoodTracker entity

---
+++ Backend/MentalHealth/Models/Entities/MoodTracker.cs
@@ -0,0 +1,36 @@
+﻿namespace MentalHealth.Models.Entities
+{
+	public class MoodTracker
+	{
+		public DateTime CreateDate;
+		public int MoodValue;
+
+		public Dictionary<int, string> EmotionsDictionary = new Dictionary<int, string>()
+		{
+			{1, "Very negative mood: " +
+				"This could include emotions such as despair, hopelessness, and extreme sadness. " +
+				"In terms of the circumplex model, this would be characterized by a strongly negative valence and relatively low arousal."
+			},
+			{2, "Mildly negative mood: " +
+				"This could include emotions such as disappointment, frustration, and mild sadness. " +
+				"In terms of the circumplex model, this would be characterized by a somewhat negative valence and relatively low arousal."
+			}, 
+			{3, "Neutral mood: " +
+				"This could include emotions such as calmness, contentment, and a lack of strong emotion. " +
+				"In terms of the circumplex model, this would be characterized by a neutral valence and relatively low arousal." 
+			},
+			{4, "Mildly positive mood: " +
+				"This could include emotions such as satisfaction, pleasure, and mild happiness. " +
+				"In terms of the circumplex model, this would be characterized by a somewhat positive valence and relatively low to moderate arousal." 
+			},
+			{5, "Very positive mood: " +
+				"This could include emotions such as joy, elation, and excitement. " +
+				"In terms of the circumplex model, this would be characterized by a strongly positive valence and relatively high arousal." 
+			},
+		};
+		public MoodTracker()
+		{
+
+		}
+	}
+}
