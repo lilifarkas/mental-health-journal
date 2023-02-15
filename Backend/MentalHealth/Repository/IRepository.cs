2706f216fcc2dd7f3486b9d39674ded3edce2a47
Attila Dobi <attiladobi100@gmail.com>
2023-02-15 19:26:29 +0100

    Removed id from Update method in IRepository interface

--- Backend/MentalHealth/Repository/IRepository.cs
+++ Backend/MentalHealth/Repository/IRepository.cs
@@ -5,6 +5,6 @@ public interface IRepository<T>
     public Task Add(T entity);
     public Task<T> Get(long id);
     public Task<IEnumerable<T>> GetAll();
-    public Task Update(long id, T entity);
+    public Task Update(T entity);
     public Task Delete(long id);
 }
