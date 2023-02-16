5e7527fd96b0466ba68f00b6758da70046834f5e
Dorian Szatmari <dorian.szatmari@gmail.com>
2023-02-16 14:31:08 +0100

    Refractor MoodTracker.cs

--- Backend/MentalHealth/Models/Entities/MoodTracker.cs
+++ Backend/MentalHealth/Models/Entities/MoodTracker.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
+using MentalHealth.Models.Enums;
 
 namespace MentalHealth.Models.Entities
 {
@@ -6,31 +7,6 @@ namespace MentalHealth.Models.Entities
 	{
 		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
 		public long ID { get; set; }
-		public DateTime CreateDate = DateTime.Now;
-		public int MoodValue { get; set; }
-
-		public Dictionary<int, string> EmotionsDictionary = new Dictionary<int, string>()
-		{
-			{1, "Very negative mood: " +
-				"This could include emotions such as despair, hopelessness, and extreme sadness. " +
-				"In terms of the circumplex model, this would be characterized by a strongly negative valence and relatively low arousal."
-			},
-			{2, "Mildly negative mood: " +
-				"This could include emotions such as disappointment, frustration, and mild sadness. " +
-				"In terms of the circumplex model, this would be characterized by a somewhat negative valence and relatively low arousal."
-			}, 
-			{3, "Neutral mood: " +
-				"This could include emotions such as calmness, contentment, and a lack of strong emotion. " +
-				"In terms of the circumplex model, this would be characterized by a neutral valence and relatively low arousal." 
-			},
-			{4, "Mildly positive mood: " +
-				"This could include emotions such as satisfaction, pleasure, and mild happiness. " +
-				"In terms of the circumplex model, this would be characterized by a somewhat positive valence and relatively low to moderate arousal." 
-			},
-			{5, "Very positive mood: " +
-				"This could include emotions such as joy, elation, and excitement. " +
-				"In terms of the circumplex model, this would be characterized by a strongly positive valence and relatively high arousal." 
-			},
-		};
+		public Moods Description { get; set; }
 	}
 }
