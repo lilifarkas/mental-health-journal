220e455f3e3687815031600c3ed5d0961fb842f9
Dorian Szatmari <dorian.szatmari@gmail.com>
2023-02-16 22:41:29 +0100

    Add MoodTrackerService.cs

---
+++ Backend/MentalHealth/Service/MoodTrackerService.cs
@@ -0,0 +1,31 @@
+﻿using MentalHealth.Models.Entities;
+
+namespace MentalHealth.Service;
+
+public class MoodTrackerService : IService<MoodTracker>
+{
+    public Task AddToDb(MoodTracker task)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<MoodTracker> GetFromDb(long id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<MoodTracker>> GetAllFromDb()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateInDb(MoodTracker task)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteFromDb(long id)
+    {
+        throw new NotImplementedException();
+    }
+}
