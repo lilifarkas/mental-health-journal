53d6e1c3ff6c63862c6861194f7449fbd2228f10
Dorian Szatmari <dorian.szatmari@gmail.com>
2023-02-16 14:30:48 +0100

    Add MoodTracker and User join table

---
+++ Backend/MentalHealth/Models/Entities/MoodTrackerUserJoin.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MentalHealth.Models.Entities;
+
+public class MoodTrackerUserJoin
+{
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public long ID { get; set; }
+    public long MoodTrackerID { get; set; }
+    public MoodTracker MoodTracker { get; set; }
+    public long UserID { get; set; }
+    public User User;
+    public DateTime CreateDate { get; set; }
+}
