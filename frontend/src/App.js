167df9220eab134c1327bf4989d56e914e8f2b70
lili.farkas <lili.farkas14@gmail.com>
2023-02-16 21:06:08 +0100

    Add profile route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,17 @@ import './App.css';
 import {Route,Routes,BrowserRouter} from 'react-router-dom';
 import MainPage from "./Components/MainPage/MainPage";
 import Footer from './Components/Footer/Footer';
+import Profile from "./Components/Profile/Profile"
 
 function App() {
   return (
     <div className='App'>
       <div className='container'>
-        <BrowserRouter>
+       < MainPage />
+       <BrowserRouter>
           <Routes>
             <Route path="/main" element={<MainPage />}></Route>
+              <Route exact path="/profile" element={<Profile />} />
           </Routes>
         </BrowserRouter>
       </div>
