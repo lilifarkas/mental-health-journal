8c2a7f6c9b5abcfba1fce5e2fff6507d9c1cf457
Dorian Szatmari <dorian.szatmari@gmail.com>
2023-02-17 02:12:34 +0100

    Change routing, navbar only displays on main page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,26 @@
-import './App.css';
-import {Route,Routes,BrowserRouter} from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import {BrowserRouter, Route,Routes, useLocation} from 'react-router-dom';
 import MainPage from "./Components/MainPage/MainPage";
 import Register from './Components/Register/Register.jsx';
 import Profile from "./Components/Profile/Profile"
 import LandingPage from "./Components/LandingPage/LandingPage";
+import Tasks from "./Components/Tasks/Tasks";
+import NavBar from './Components/NavBar/NavBar';
 
 function App() {
+  const location = useLocation();
+  const excludedPaths = ['/', '/registration'];
+
   return (
-    <div className='App'>
-      <div className='container'>
-       <BrowserRouter>
-          <Routes>
-              <Route path='/' element={<LandingPage />} />
-            <Route path='/registration' element={<Register />} />
-            <Route path="/main" element={<MainPage />}></Route>
-              <Route exact path="/profile" element={<Profile />} />
-          </Routes>
-        </BrowserRouter>
-      </div>
+    <div>
+        { !excludedPaths.includes(location.pathname) && <NavBar /> }
+        <Routes>
+          <Route path='/' element={<LandingPage />} />
+          <Route path='/registration' element={<Register />} />
+          <Route path="/main" element={<MainPage />}></Route>
+          <Route exact path="/profile" element={<Profile />} />
+          <Route path='/tasks' element={<Tasks/>}></Route>
+        </Routes>
     </div>
   );
 }
