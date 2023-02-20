73801371a67b88fd47ad0aeac922e2184dcc5856
Akos <barcza.akos15@gmail.com>
2023-02-20 19:32:02 +0100

    Undo previous changes

--- frontend/src/Components/Emojis/EmojiAngry/EmojiAngry.jsx
+++ frontend/src/Components/Emojis/EmojiAngry/EmojiAngry.jsx
@@ -1,12 +1,10 @@
 import { BsEmojiAngryFill } from 'react-icons/bs';
-
 export default function EmojiAngry(props) {
-
     function handleClick() {
         props.passedProp(props.value);
     }
 
     return (
-        <BsEmojiAngryFill onClick={handleClick} />
+            <BsEmojiAngryFill onClick={handleClick}/>
     )
 }
