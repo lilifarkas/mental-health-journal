397cd2a3551c13d6a203995b29f0bb65d4e5b97f
Akos <barcza.akos15@gmail.com>
2023-02-21 20:22:12 +0100

    Add Login to Routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import {BrowserRouter, Route,Routes, useLocation} from 'react-router-dom';
+import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom';
 import MainPage from "./Components/MainPage/MainPage";
 import Register from './Components/Register/Register.jsx';
 import Profile from "./Components/Profile/Profile"
 import LandingPage from "./Components/LandingPage/LandingPage";
 import Tasks from "./Components/Tasks/Tasks";
 import NavBar from './Components/NavBar/NavBar';
+import Login from './Components/Login/Login.jsx';
 
 function App() {
   const location = useLocation();
@@ -13,14 +14,15 @@ function App() {
 
   return (
     <div>
-        { !excludedPaths.includes(location.pathname) && <NavBar /> }
-        <Routes>
-          <Route path='/' element={<LandingPage />} />
-          <Route path='/registration' element={<Register />} />
-          <Route path="/main" element={<MainPage />}></Route>
-          <Route exact path="/profile" element={<Profile />} />
-          <Route path='/tasks' element={<Tasks/>}></Route>
-        </Routes>
+      {!excludedPaths.includes(location.pathname) && <NavBar />}
+      <Routes>
+        <Route path='/' element={<LandingPage />} />
+        <Route path='/registration' element={<Register />} />
+        <Route path="/main" element={<MainPage />}></Route>
+        <Route exact path="/profile" element={<Profile />} />
+        <Route path='/tasks' element={<Tasks />}></Route>
+        <Route path='/login' element={<Login />} />
+      </Routes>
     </div>
   );
 }
