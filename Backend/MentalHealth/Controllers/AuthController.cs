e387a0aff161339cd815955eab4e163817dfae2e
Dorian Szatmari <dorian.szatmari@gmail.com>
2023-03-01 15:39:59 +0100

    Add register endpoint to AuthController.cs

---
+++ Backend/MentalHealth/Controllers/AuthController.cs
@@ -0,0 +1,31 @@
+﻿using MentalHealth.Models.DTOs;
+using MentalHealth.Models.Entities;
+using MentalHealth.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MentalHealth.Controllers;
+
+[Route("/welcome")]
+[ApiController]
+public class AuthController : ControllerBase
+{
+    private readonly UserService _service;
+    public static User? User = new User();
+    
+    public AuthController(UserService service)
+    {
+        _service = service;
+    }
+    
+    [HttpPost("/register")]
+    public async Task<ActionResult<User>> Register([FromBody] RegisterDTO registerDto)
+    {
+        User.Name = registerDto.Name;
+        User.Email = registerDto.Email;
+        User.Password = registerDto.Password;
+        
+        await _service.Add(User);
+        return Ok(User);
+    }
+
+}
