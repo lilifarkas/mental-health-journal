b94a3b20f642e245ff4ae64fc04650e90c2c58a4
Dorian Szatmari <dorian.szatmari@gmail.com>
2023-03-02 16:05:32 +0100

    Add login redirect to main page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect, Outlet } from 'react';
-import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Route, Routes, useNavigate } from 'react-router-dom';
 import MainPage from "./Components/MainPage/MainPage";
 import Register from './Components/Register/Register.jsx';
 import Profile from "./Components/Profile/Profile"
@@ -12,33 +12,35 @@ import jwt_decode from "jwt-decode";
 import ProtectedRoute from './Components/Auth/ProtectedRoute';
 
 function App() {
+  const navigate = useNavigate();
   const [user, setUser] = useState(null);
 
   const handleLogin = (token) => {
     localStorage.setItem("jwtToken", token);
     setUser(jwt_decode(token));
+    navigate('/main', { replace: true });
   };
   const handleLogout = () => {
     localStorage.removeItem("jwtToken");
     setUser(null);
+    navigate('/', { replace: true });
   };
 
   return (
     <>
       {user && <NavBar handleLogout={handleLogout} user={user}/>}
-
       <Routes>
-        <Route index element ={<LandingPage />} />
-        <Route path='/registration' element={<Register />} />
 
         <Route element={<ProtectedRoute user={user}/>}>
           <Route path='/main' element = {<MainPage/>}/>
-          <Route exact path="/profile" element={<Profile />} />
-          <Route exact path="/profile/edit/:id" element={<EditProfile />} />
-          <Route path='/tasks' element={<Tasks />}></Route>
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/profile/edit/:id" element={<EditProfile />} />
+          <Route path='/tasks' element={<Tasks />}/>
         </Route>     
         
-        <Route path='/login' element={<Login onLogin={handleLogin}/>} />
+      {!user && (<><Route path='/login' element={<Login onLogin={handleLogin}/>} />
+        <Route index element ={<LandingPage />} />
+        <Route path='/registration' element={<Register />} /></>)}
       </Routes>
     </>
   );
