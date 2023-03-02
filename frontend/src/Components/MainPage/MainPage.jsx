fc4b165eef903684763ffd1734a9f38563ab844d
Attila Dobi <attiladobi100@gmail.com>
2023-03-02 13:15:23 +0100

    Added Garden to tha MainPage

--- frontend/src/Components/MainPage/MainPage.jsx
+++ frontend/src/Components/MainPage/MainPage.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import NavBar from "../NavBar/NavBar";
 import Footer from "../Footer/Footer";
 import MoodTracker from '../MoodTracker/MoodTracker';
+import Garden from '../Garden/Garden';
 import "./MainPage.css";
 
 function MainPage() {
     return (
         <div className='container'>
             {/* < NavBar /> */}
+            < Garden />
             < MoodTracker />
             {/* < Footer /> */}
         </div>
