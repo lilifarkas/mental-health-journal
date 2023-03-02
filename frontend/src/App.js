68a0a0aa04c0d7a4ef77c5db3182ea7897c571e2
Dorian Szatmari <dorian.szatmari@gmail.com>
2023-03-02 13:14:07 +0100

    Add protected routes to app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, Outlet } from 'react';
 import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom';
 import MainPage from "./Components/MainPage/MainPage";
 import Register from './Components/Register/Register.jsx';
@@ -8,24 +8,39 @@ import Tasks from "./Components/Tasks/Tasks";
 import NavBar from './Components/NavBar/NavBar';
 import Login from './Components/Login/Login.jsx';
 import EditProfile from './Components/Profile/EditProfile'
+import jwt_decode from "jwt-decode";
+import ProtectedRoute from './Components/Auth/ProtectedRoute';
 
 function App() {
-  const location = useLocation();
-  const excludedPaths = ['/', '/registration', '/login'];
+  const [user, setUser] = useState(null);
+
+  const handleLogin = (token) => {
+    localStorage.setItem("jwtToken", token);
+    setUser(jwt_decode(token));
+  };
+  const handleLogout = () => {
+    localStorage.removeItem("jwtToken");
+    setUser(null);
+  };
 
   return (
-    <div>
-      {!excludedPaths.includes(location.pathname) && <NavBar />}
+    <>
+      {user && <NavBar handleLogout={handleLogout} user={user}/>}
+
       <Routes>
-        <Route path='/' element={<LandingPage />} />
+        <Route index element ={<LandingPage />} />
         <Route path='/registration' element={<Register />} />
-        <Route path="/main" element={<MainPage />}></Route>
-        <Route exact path="/profile" element={<Profile />} />
-        <Route exact path="/profile/edit/:id" element={<EditProfile />} />
-        <Route path='/tasks' element={<Tasks />}></Route>
-        <Route path='/login' element={<Login />} />
+
+        <Route element={<ProtectedRoute user={user}/>}>
+          <Route path='/main' element = {<MainPage/>}/>
+          <Route exact path="/profile" element={<Profile />} />
+          <Route exact path="/profile/edit/:id" element={<EditProfile />} />
+          <Route path='/tasks' element={<Tasks />}></Route>
+        </Route>     
+        
+        <Route path='/login' element={<Login onLogin={handleLogin}/>} />
       </Routes>
-    </div>
+    </>
   );
 }
 
