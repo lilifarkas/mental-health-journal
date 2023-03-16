7221b103a5e6d1f0458ccfbcb92eb3ab0be3178c
Attila Dobi <attiladobi100@gmail.com>
2023-03-16 16:57:59 +0100

    Changed Id type to long

--- Backend/MentalHealth/Models/Entities/DefaultTask.cs
+++ Backend/MentalHealth/Models/Entities/DefaultTask.cs
@@ -5,7 +5,9 @@ namespace MentalHealth.Models.Entities;
 public class DefaultTask
 {
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-    public int ID { get; set; }
+    public long ID { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
+    
+    public int Point { get; set; }
 }
